24fc09db6c2dd5330a60483d6abf9c7a179c590e
Syed Muhammad Hussain <97514703+hussain1406@users.noreply.github.com>
2023-02-10 12:41:11 +0500

    Fixed some bugs & added clients folder

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
+# change the directory to main.py
+current_dir = os.path.dirname(__file__)
+os.chdir(current_dir)
+if not os.path.exists("clients"):
+    os.mkdir("clients")
 # to change people edit this list
 clients = ['harry', 'rohan', 'hammad', 'hussain']
 # currently only these two operations are supported
 operations = ['create a log', 'retrieve from a log']
+# types of log - can be changed and more can be added
 types = ['food', 'exercise']
 messages = ['What operation do you want to perform?:',
             'For which client do you want to add info?',
-            'Food or Exercise?']
+            'Food or Exercise?']  # messages printed before the list of options
 choice_of_usr = 0
 
 
@@ -13,11 +20,11 @@ def main_function():
     print("\t\t\t\t-------------------------------------")
     print("\t\t\t\t Welcome to Health Management System")
     print("\t\t\t\t-------------------------------------")
-    operations_ans = operation_manager(0, operations)
-    clients_ans = operation_manager(1, clients)
-    types_ans = operation_manager(2, types)
+    operations_ans = choice(0, operations)
+    clients_ans = choice(1, clients)
+    types_ans = choice(2, types)
     if operations_ans == 0:
-        with open(f"{clients[clients_ans]}-{types[types_ans]}.txt", "a") as logfile:
+        with open(f"./clients/{clients[clients_ans]}-{types[types_ans]}.txt", "a") as logfile:
             log = str(input(f"What did {clients[clients_ans].capitalize()} eat? " if types_ans ==
                       0 else f"What exercise did {clients[clients_ans].capitalize()} do? "))
             if log == '':
@@ -26,7 +33,7 @@ def main_function():
                 quit()
             timestamp = getDateTime()
             log = f"[ {timestamp} ] " + log
-            logfile.write(log)
+            logfile.write(log + "\n")
             # print(log)
     else:
         try:
@@ -39,20 +46,9 @@ def main_function():
                 print("\n\t\t\tEND OF FILE\n")
                 print("----------------------------------------------------------------")
         except:
-            print(f"There are no logs for {clients[clients_ans].capitalize()}")
+            print(
+                f"There are no logs in {types[types_ans]} for {clients[clients_ans].capitalize()}")
     return "Operation Successful"
-
-
-def operation_manager(choice_maker, operand):
-    """Takes two parameters.
-
-    choice_maker is list containing data as parameter.
-
-    choice_maker takes 1 or 0. 0 means operation 1 means clients 2 means type (either food or exercise) in case of operation_function
-    """
-    print_options(choice_maker, operand)
-    ans = choice(choice_maker, operand)
-    return ans
 
 
 def choice(type_of_inp, operand):
@@ -63,13 +59,12 @@ def choice(type_of_inp, operand):
     """
     global choice_of_usr  # initialized at the end of the file
     types_of_inp = ["operation", "client", "type"]
-    if str(choice_of_usr) == 'q' or str(choice_of_usr) == '':
-        print("The Program will exit now...")
     main_choice = types_of_inp[type_of_inp]
+    print_options(type_of_inp, operand)
     choice_of_usr = str(input(
         "\nPlease choose one of the options above: (type the "+main_choice+" name or "+main_choice+" number): "))
 
-    def check_choice(choice_maker, operand):
+    def validate_choice(choice_maker, operand):
         try:
             if int(choice_of_usr) <= len(operand):
                 return int(choice_of_usr) - 1
@@ -80,19 +75,19 @@ def choice(type_of_inp, operand):
         except Exception as e:
             if choice_of_usr == 'q' or choice_of_usr == '':
                 print("The Program will exit now...")
-                return -1
+                print("Program Exited successfully")
+                quit()
+
             found = False
-            if found == False:
-                for i in range(0, len(operand)):
-                    if choice_of_usr.lower() == operand[i]:
-                        found = True
-                        return i
-            else:
+            for i in range(0, len(operand)):
+                if choice_of_usr.lower() == operand[i]:
+                    found = True
+                    return i
+            if not found:
                 print("You entered an incorrect name. try again!")
-                print_options(operand)
                 return choice(choice_maker, operand)
-    ans = check_choice(type_of_inp, operand)
-    check_status(ans)
+
+    ans = validate_choice(type_of_inp, operand)
     return ans
 
 
@@ -102,12 +97,6 @@ def print_options(choice_maker, operand):
     print('\n')
     for x in range(len(operand)):
         print(f"\t{x+1}. {operand[x].capitalize()}")
-
-
-def check_status(status):
-    if status == -1:
-        print("Program Exited successfully")
-        quit()
 
 
 def getDateTime():
